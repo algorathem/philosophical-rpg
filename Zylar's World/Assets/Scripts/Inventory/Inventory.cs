e83035c578237a1eedb9c7813ef68ebaba3140e6
MingKaiDev <47216870+MingKaiDev@users.noreply.github.com>
2025-01-12 23:51:19 +0800

    Inventory Version 1

---
+++ Zylar's World/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,192 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static UnityEditor.Progress;
+using static UnityEngine.InputManagerEntry;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeReference] public List<ItemSlotInfo> items = new List<ItemSlotInfo>();
+    [Space]
+    [Header("Inventory Menu Components")]
+    public GameObject inventoryMenu;
+    public GameObject itemPanel;
+    public GameObject itemPanelGrid;
+
+    public Mouse mouse;
+
+    private List<ItemPanel> existingPanels = new List<ItemPanel>();
+
+    Dictionary<string, Item> allItemsDictionary = new Dictionary<string, Item>();
+
+    [Space]
+    public int inventorySize = 18;
+    void Start()
+    {
+        for (int i = 0; i < inventorySize; i++)
+        {
+            items.Add(new ItemSlotInfo(null, 0));
+        }
+
+        List<Item> allItems = GetAllItems().ToList();
+        string itemsInDictionary = "Items in Dictionary: ";
+        foreach (Item i in allItems)
+        {
+            if (!allItemsDictionary.ContainsKey(i.GiveName()))
+            {
+                allItemsDictionary.Add(i.GiveName(), i);
+                itemsInDictionary += ", " + i.GiveName();
+            }
+            else
+            {
+                Debug.Log("" + i + " already exists in Dictionary - shares name with " + allItemsDictionary[i.GiveName()]);
+            }
+        }
+        itemsInDictionary += ".";
+        Debug.Log(itemsInDictionary);
+
+        AddItem("Wood", 40);
+        AddItem("Stone", 22);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (inventoryMenu.activeSelf)
+            {
+                inventoryMenu.SetActive(false);
+                mouse.EmptySlot();
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            else
+            {
+                inventoryMenu.SetActive(true);
+                Cursor.lockState = CursorLockMode.Confined;
+                RefreshInventory();
+            }
+        }
+        
+    }
+
+    public void RefreshInventory()
+    {
+        existingPanels = itemPanelGrid.GetComponentsInChildren<ItemPanel>().ToList();
+        if (existingPanels.Count < inventorySize)
+        {
+            int amountToCreate = inventorySize - existingPanels.Count;
+            for (int i = 0; i < amountToCreate;i++)
+            {
+                GameObject newPanel = Instantiate(itemPanel, itemPanelGrid.transform);
+                existingPanels.Add(newPanel.GetComponent<ItemPanel>());
+            }
+        }
+
+        int index = 0;
+        foreach (ItemSlotInfo i in items)
+        {
+            //Name out List Elementals
+            i.name = "" + (index + 1);
+            if (i.item != null) i.name += ": " + i.item.GiveName();
+            else i.name += ": -";
+
+            //Update our Panels
+            ItemPanel panel = existingPanels[index];
+            panel.name = i.name + " Panel";
+            if(panel != null)
+            {
+                panel.inventory = this;
+                panel.itemSlot = i;
+                if (i.item != null)
+                {
+                    panel.itemImage.gameObject.SetActive(true);
+                    panel.itemImage.sprite = i.item.GiveItemImage();
+                    panel.itemImage.CrossFadeAlpha(1, 0.05f, true);
+                    panel.stacksText.gameObject.SetActive(true);
+                    panel.stacksText.text = "" + i.stacks;
+                }
+                else
+                {
+                    panel.itemImage.gameObject.SetActive(false);
+                    panel.stacksText.gameObject.SetActive(false);
+                }
+            }
+            index++;
+        }
+        mouse.EmptySlot();
+    }
+
+    public int AddItem(string itemName, int amount)
+    {
+        // Find Item to add
+        Item item = null;
+        allItemsDictionary.TryGetValue(itemName, out item);
+        //Exit method if no Item was found
+        if (item == null)
+        {
+            Debug.Log("Could not find Item in Dictionary to add to Inventory");
+            return amount;
+        }
+        //Check for open spaces in existing slots
+        foreach (ItemSlotInfo i in items)
+        {
+            if (i.item != null)
+            {
+                if (i.item.GiveName() == item.GiveName())
+                {
+                    if (amount > i.item.MaxStacks() - i.stacks)
+                    {
+                        amount -= i.item.MaxStacks() - i.stacks;
+                        i.stacks = i.item.MaxStacks();
+                    }
+                    else
+                    {
+                        i.stacks += amount;
+                        if (inventoryMenu.activeSelf) RefreshInventory();
+                        return 0;
+                    }
+                }
+            }
+        }
+        //Fill empty slots with leftover items
+        foreach (ItemSlotInfo i in items)
+        {
+            if (i.item == null)
+            {
+                if (amount > item.MaxStacks())
+                {
+                    i.item = item;
+                    i.stacks = item.MaxStacks();
+                    amount -= item.MaxStacks();
+                }
+                else
+                {
+                    i.item = item;
+                    i.stacks = amount;
+                    if (inventoryMenu.activeSelf) RefreshInventory();
+                    return 0;
+                }
+            }
+        }
+        //No space in Inventory, return remainder items
+        Debug.Log("No space in Inventory for: " + item.GiveName());
+        if (inventoryMenu.activeSelf) RefreshInventory();
+        return amount;
+    }
+
+    public void ClearSlot(ItemSlotInfo slot)
+    {
+        slot.item = null;
+        slot.stacks = 0;
+    }
+
+    IEnumerable<Item> GetAllItems()
+    {
+        return System.AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes()).Where(type => type.IsSubclassOf(typeof(Item)))
+            .Select(type => System.Activator.CreateInstance(type) as Item);
+    }
+}
+
