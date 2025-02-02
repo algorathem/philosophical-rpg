0863f4f38a7247847cfe146d32ed35792f699783
Oscar Qian <91544028+oscarqjh@users.noreply.github.com>
2025-02-02 11:17:51 +0800

    Revert "Plato's cave"

--- Zylar's World/Assets/Scripts/CharacterControl/Data/PlayerRollData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System;
-
-[Serializable]
-public class PlayerRollData
-{
-    [field: SerializeField][field: Range(0f, 3f)] public float speedMultiplier { get; private set; } = 1f;
-}
