2a3dcee202e5ecfe6c49664afb90055add91ae44
Liu Yanzhi <164533817+Liuyzaz@users.noreply.github.com>
2025-04-12 02:19:58 +0800

    add start scene, ending1 scene and skip keyboard shortcut

--- Zylar's World/Assets/Scripts/YarnSceneLoader.cs
+++ Zylar's World/Assets/Scripts/YarnSceneLoader.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Yarn.Unity;
@@ -10,5 +9,19 @@ public class YarnSceneLoader : MonoBehaviour
     public static void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    [YarnCommand("fadeAndLoad")]
+    public static void FadeAndLoad(string sceneName)
+    {
+        SceneFader fader = GameObject.FindObjectOfType<SceneFader>();
+        if (fader != null)
+        {
+            fader.StartCoroutine(fader.FadeOutAndLoadScene(sceneName));
+        }
+        else
+        {
+            Debug.LogWarning("SceneFader not found in scene!");
+        }
     }
 }
